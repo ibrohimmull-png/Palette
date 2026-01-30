c50527cf6b048081952d7237e261fb8ad88b1ae6
Ibrohim <ibrohimmull@gmail.com>
2026-01-30 18:32:04 +0500

    palette

---
+++ main.js
@@ -0,0 +1,33 @@
+let colors = ["blue", "red", "green", "yellow"];
+
+    let boxes = document.querySelectorAll(".box");
+    let boxPalette = document.getElementById("boxPalette");
+    let bgPalette = document.getElementById("bgPalette");
+
+    
+    for (let i = 0; i < colors.length; i++) {
+      let colorDiv = document.createElement("div");
+      colorDiv.className = "color";
+      colorDiv.style.backgroundColor = colors[i];
+
+      colorDiv.onclick = function () {
+        boxes.forEach(function (box) {
+          box.style.backgroundColor = colors[i];
+        });
+      };
+
+      boxPalette.appendChild(colorDiv);
+    }
+
+    
+    for (let i = 0; i < colors.length; i++) {
+      let colorDiv = document.createElement("div");
+      colorDiv.className = "color";
+      colorDiv.style.backgroundColor = colors[i];
+
+      colorDiv.onclick = function () {
+        document.body.style.backgroundColor = colors[i];
+      };
+
+      bgPalette.appendChild(colorDiv);
+    } 
